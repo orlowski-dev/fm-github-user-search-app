46770959e2b49d9b75092145fa086a6fe0f18d23
orlowski-dev <orlowski.dev@gmail.com>
2023-07-22 07:03:20 +0200

    01 - Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.scss'
+
+export default function App() {
+  return (<>
+    <header>
+      <h1>devfinder</h1>
+      <h2>Github user search app</h2>
+    </header>
+  </>)
+}
---
+++ src/assets/index.tsx
@@ -0,0 +1,3 @@
+export { default as moonIcon } from './svg/icon-moon.svg'
+export { default as sunIcon } from './svg/icon-sun.svg'
+export { default as searchIcon } from './svg/icon-search.svg'
