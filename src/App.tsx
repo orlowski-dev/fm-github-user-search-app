106aa3a62c82c10543840e2735deb084ceceabb7
orlowski-dev <orlowski.dev@gmail.com>
2023-07-23 18:27:09 +0200

    03 - Added searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.scss'
-import { moonIcon, sunIcon } from './assets'
+import { moonIcon, searchIcon, sunIcon } from './assets'
 
 const getColorScheme = (): string => {
   const colorSchemeInLocalStorage = localStorage.getItem('colorScheme')
@@ -25,7 +25,7 @@ export default function App() {
   return (<>
     <header className='main-header container d-fl ai-c jc-sb'>
       <div className="brand-area">
-        <span>devfinder</span>
+        <span className={colorScheme === 'dark' ? 'dark' : undefined}>devfinder</span>
       </div>
       <div className="button-area">
         <button
@@ -37,5 +37,27 @@ export default function App() {
         </button>
       </div>
     </header>
+    <main>
+      <section className={`searchbar-section container w-bg ${colorScheme}`}>
+        <h2 className='visually-hidden'>Searchbar section</h2>
+        <form>
+          <img src={searchIcon} alt='search icon' />
+          <label>
+            <span className="visually-hidden">GitHub username</span>
+            <input
+              type="text"
+              id="username"
+              placeholder="Search GitHub username.."
+              className={colorScheme === 'dark' ? 'dark' : undefined}
+            />
+          </label>
+          <span
+            className="error"
+            id='form-error'
+          ></span>
+          <button>Search</button>
+        </form>
+      </section>
+    </main>
   </>)
 }
